976013300bf7c841d392fae3554f59d3b627683e
Saiteja-21 <saitejakatkam123@gmail.com>
2023-02-20 23:52:56 -0500

    added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,40 @@
 import { useEffect, useState } from "react";
+//import { useState } from "react";
 import "./Navbar.css";
+
+
 
 const Navbar = () => {
   const [show, setShow] = useState(false);
-  useEffect(() => {
+  const changenavbarcolor = ()  =>{
+   console.log(window.scrollY);
+    if(window.scrollY>80){
+      setShow(true);
+      
+    }
+    else{
+      setShow(false);
+    }
+   }
 
-  }, []);
+   useEffect(() => {
+  
+    window.addEventListener('scroll',changenavbarcolor)
 
+     },[show]);
+  console.log(window.scrollY);
   return (
+   
+    <div className={show?'nav__black':'nav'}>
+  
+      <li ><img className="nav__logo" src='./netflix-logo.jpg' alt='not found'></img></li>
+      <li ><img className="nav__avatar"  src='./Netflix-avatar.png' alt='notfound'></img></li>
+     
+
+    </div>
+ 
+  
+ 
    
   );
 };
